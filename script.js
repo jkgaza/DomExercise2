abc50831de3d0a2bea9cc0ca94b0caff8bf45cf7
Jordan Gaza <Gaza>
2019-12-05 15:26:31 -0500

    Added if/else if/else statement

--- script.js
+++ script.js
@@ -1,36 +1,47 @@
-'use strict';
+"use strict";
 
 {
-    const addFoodForm = document.querySelector('#add-food');
+  const addFoodForm = document.querySelector("#add-food");
 
-    addFoodForm.addEventListener('submit', e => {
-        e.preventDefault();
+  addFoodForm.addEventListener("submit", e => {
+    e.preventDefault();
 
-        const itemToAdd = document.querySelector('#name');
+    const itemToAdd = document.querySelector("#name");
+    
+    if (itemToAdd.value && isNaN(itemToAdd.value)) {
+      // <li></li>
+      const itemHTML = document.createElement("li");
 
-        if (itemToAdd.value && isNaN(itemToAdd.value)) {
+      // <li class="list-item"></li>
+      itemHTML.classList.add("list-item");
+
+      // <li class="list-item"> Text from the input in Here </li>
+      itemHTML.innerText = itemToAdd.value;
+
+    //   itemHTML.addEventListener("click", e => {
+    //     itemHTML.classList.toggle("completed");
+    //   });
+
+      const addFruit = document.querySelector("#fruitlist");
+      addFruit.appendChild(itemHTML);
+      itemToAdd.value = "";
+//ADDITION STARTS HERE
+    } else if (itemToAdd.value && isNaN(itemToAdd.value)) {
             // <li></li>
-            const itemHTML = document.createElement('li');
-
+            const itemHTML = document.createElement("li");
+      
             // <li class="list-item"></li>
-            itemHTML.classList.add('list-item');
-
+            itemHTML.classList.add("list-item");
+      
             // <li class="list-item"> Text from the input in Here </li>
             itemHTML.innerText = itemToAdd.value;
-
-            itemHTML.addEventListener('click', e => {
-                itemHTML.classList.toggle('completed');
-            })
-
-           
-
-            const wishList = document.querySelector('#fruitlist');
-
-            wishList.appendChild(itemHTML);
-            itemToAdd.value = ''; 
-        } else {
-            alert("You didn't type anything you jerk");
-        }
-
-    });
+      
+            const addVeggie = document.querySelector("#veggielist");
+            addVeggie.appendChild(itemHTML);
+            itemToAdd.value = "";
+//ADDITION ENDS HERE
+    } else {
+      alert("You didn't type anything you jerk");
+    }
+  });
 }
