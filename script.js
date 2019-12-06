ce11f52ede4820db9dff7270f0d89487f3481e36
Jordan Gaza <Gaza>
2019-12-06 14:07:56 -0500

    Finished code, adding nested if loop to add items to veg and fruit columns, THANKS BJ

--- script.js
+++ script.js
@@ -17,25 +17,20 @@
       // <li class="list-item"> Text from the input in Here </li>
       itemHTML.innerText = itemToAdd.value;
 
-      const addFruit = document.querySelector("#fruitlist");
+      let checkedRadio = document.querySelector('[name="type"]:checked');
+
+      if (checkedRadio.value == "fruit"){
+        const addFruit = document.querySelector("#fruitlist");
       addFruit.appendChild(itemHTML);
+      } else {
+        const addVeggie = document.querySelector("#veggielist");
+      addVeggie.appendChild(itemHTML);
+      }
+
+      
       itemToAdd.value = "";
-//ADDITION STARTS HERE
-    } else if (itemToAdd.value && isNaN(itemToAdd.value)) {
-            // <li></li>
-            const itemHTML = document.createElement("li");
-      
-            // <li class="list-item"></li>
-            itemHTML.classList.add("list-item");
-      
-            // <li class="list-item"> Text from the input in Here </li>
-            itemHTML.innerText = itemToAdd.value;
-      
-            const addVeggie = document.querySelector("#veggielist");
-            addVeggie.appendChild(itemHTML);
-            itemToAdd.value = "";
-//ADDITION ENDS HERE
-    } else {
+
+    }  else {
       alert("You didn't type anything you jerk");
     }
   });
